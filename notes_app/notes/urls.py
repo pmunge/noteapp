14c6bbc946d027818c843aeefb58307759982269
pmunge <munge.pw620@gmail.com>
2024-09-30 10:31:21 +0300

    made changes to views.py

--- notes_app/notes/urls.py
+++ notes_app/notes/urls.py
@@ -3,10 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_routes, name='routes'),
-    path('notes/', views.get_notes_list, name='notes'),
-    path('notes/<str:pk>/', views.get_note_detail, name='note'),
-    path('notes/create/', views.create_note, name='create-note'),
-    path('notes/update/<str:pk>/', views.update_note, name='update-note'),
-    path('notes/delete/<str:pk>/', views.delete_note, name='delete-note'),
+    path('', views.getRoutes, name='routes'),
+    path('notes/',views.getNotes,name='notes'),
+    path('notes/<str:pk>/', views.getNote, name='note')
 ]
